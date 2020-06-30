4d53a8937fdc57c159406c314eab3c1cbf1cded8
hiroyuki <hiroyuki@mbp15.local>
2020-06-30 23:39:53 +0900

    Fix INDATA_PATH

--- trigger.py
+++ trigger.py
@@ -2,14 +2,13 @@ import os
 from logics.logic import convert_data
 
 
-INDATA_PATH = os.environ.get('GITHUB_WORKSPACE') + "/data" + "/country-json" + "/src"
+INDATA_PATH = os.environ.get('GITHUB_WORKSPACE') + "/data" + "/src"
 FILENAME_C_ELEVATION = "country-by-elevation.json"
 FILENAME_C_EXPECTANCY = "country-by-life-expectancy.json"
 OUTFILE_PATH = 'output/data.json'
 
 if __name__ == '__main__':
     convert_data(
-        is_infile_json_lines=True,
         infile_elv_path=INDATA_PATH + "/" + FILENAME_C_ELEVATION,
         infile_exp_path=INDATA_PATH + "/" + FILENAME_C_EXPECTANCY,
         outfile_path=OUTFILE_PATH)
--- try.py
+++ try.py
@@ -8,7 +8,6 @@ OUTFILE_PATH = 'trydata/output/data.json'
 
 if __name__ == '__main__':
     convert_data(
-        is_infile_json_lines=False,
         infile_elv_path=INDATA_PATH + "/" + FILENAME_C_ELEVATION,
         infile_exp_path=INDATA_PATH + "/" + FILENAME_C_EXPECTANCY,
         outfile_path=OUTFILE_PATH)

eb345a9cfcffe31813ddabe11742ea8a3f3f1d37
hiroyuki <hiroyuki@mbp15.local>
2020-06-30 23:28:09 +0900

    Check actual parameters

--- logics/logic.py
+++ logics/logic.py
@@ -3,10 +3,14 @@ import pandas as pd
 
 
 def convert_data(is_infile_json_lines, infile_elv_path, infile_exp_path, outfile_path):
-    dat_elv = pd.read_json(infile_elv_path, encoding='utf_8', lines=is_infile_json_lines)
-    dat_exp = pd.read_json(infile_exp_path, encoding='utf_8', lines=is_infile_json_lines)
+    print(is_infile_json_lines)
+    print(infile_elv_path)
+    print(infile_exp_path)
+    print(outfile_path)
+    # dat_elv = pd.read_json(infile_elv_path, encoding='utf_8', lines=is_infile_json_lines)
+    # dat_exp = pd.read_json(infile_exp_path, encoding='utf_8', lines=is_infile_json_lines)
 
-    converted = pd.merge(dat_elv, dat_exp, on='country').dropna(how='any').copy()
-    converted['dev_expectancy'] = converted['expectancy'] - converted['expectancy'].mean()
-    converted['dev_elevation'] = converted['elevation'] - converted['elevation'].mean()
-    converted.to_json(outfile_path, orient='records')
+    # converted = pd.merge(dat_elv, dat_exp, on='country').dropna(how='any').copy()
+    # converted['dev_expectancy'] = converted['expectancy'] - converted['expectancy'].mean()
+    # converted['dev_elevation'] = converted['elevation'] - converted['elevation'].mean()
+    # converted.to_json(outfile_path, orient='records')
